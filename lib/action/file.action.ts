d55f069c27ed292b8f830aeb41e744178542e1b9
Your Name~ <hmphtetminpaing@gmail.com>
2024-12-09 15:24:03 +0630

    Dynamic routes

--- lib/action/file.action.ts
+++ lib/action/file.action.ts
@@ -3,9 +3,10 @@
 import { createAdminClient } from "../appwrite";
 import { InputFile } from "node-appwrite/file"
 import { appwriteConfig } from "../appwrite/config";
-import { ID } from "node-appwrite";
+import { ID, Models, Query } from "node-appwrite";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
+import { getCurrentUser } from "./user.action";
 
 const handleError = (error: unknown, message: string) => {
     console.log(error, message);
@@ -21,7 +22,7 @@ export const uploadFile = async ({
     const { storage, databases } = await createAdminClient();
     try {
         const inputFile = InputFile.fromBuffer(file, file.name);
-        
+
         const bucketFile = await storage.createFile(
             appwriteConfig.bucketId,
             ID.unique(),
@@ -41,18 +42,52 @@ export const uploadFile = async ({
         };
 
         const newFile = await databases
-        .createDocument(
-            appwriteConfig.databaseId,
-            appwriteConfig.filesCollectionId,
-            ID.unique(),
-            fileDocument,
-        ).catch(async(error: unknown) => {
-            await storage.deleteFile(appwriteConfig.bucketId, bucketFile.$id);
-            handleError(error, "Failed to create file Document");
-        })
+            .createDocument(
+                appwriteConfig.databaseId,
+                appwriteConfig.filesCollectionId,
+                ID.unique(),
+                fileDocument,
+            ).catch(async (error: unknown) => {
+                await storage.deleteFile(appwriteConfig.bucketId, bucketFile.$id);
+                handleError(error, "Failed to create file Document");
+            })
         revalidatePath(path);
         return parseStringify(newFile);
     } catch (error) {
         handleError(error, "Failed to Upload File");
+    }
+}
+
+const createQueries = (currentUser: Models.Document) => {
+    const queries = [
+        Query.or([
+            Query.equal("owner", [currentUser.$id]),
+            Query.contains("users", [currentUser.email])
+        ]),
+    ];
+    // TODO: Search, sort, limits 
+    return queries;
+}
+
+export const getFiles = async () => {
+    const { databases }= await createAdminClient();
+
+    try {
+        const currentUser = await getCurrentUser();
+        
+        if(!currentUser) throw new Error("User not found.");
+        
+        const queries = createQueries(currentUser);
+
+        const files = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.filesCollectionId,
+            queries,
+        );
+        console.log({files})
+
+        return parseStringify(files);
+    } catch (error) {
+        handleError(error, "Failed to get files");
     }
 }
